acdd561c08d5ce6212eda29a608a2d30ff1adefe
turston-l <louwturston@gmail.com>
2022-02-18 16:48:29 +0200

    creating the view ticket page

---
+++ view.php
@@ -0,0 +1,76 @@
+<?php
+include 'functions.php';
+// Connect to MySQL using the below function
+$pdo = pdo_connect_mysql();
+// Check if the ID param in the URL exists
+if (!isset($_GET['id'])) {
+    exit('No ID specified!');
+}
+// MySQL query that selects the ticket by the ID column, using the ID GET request variable
+$stmt = $pdo->prepare('SELECT * FROM tickets WHERE id = ?');
+$stmt->execute([ $_GET['id'] ]);
+$ticket = $stmt->fetch(PDO::FETCH_ASSOC);
+// Check if ticket exists
+if (!$ticket) {
+    exit('Invalid ticket ID!');
+}
+
+// Update status
+if (isset($_GET['status']) && in_array($_GET['status'], array('open', 'closed', 'resolved'))) {
+    $stmt = $pdo->prepare('UPDATE tickets SET status = ? WHERE id = ?');
+    $stmt->execute([ $_GET['status'], $_GET['id'] ]);
+    header('Location: view.php?id=' . $_GET['id']);
+    exit;
+}
+
+// Check if the comment form has been submitted
+if (isset($_POST['msg']) && !empty($_POST['msg'])) {
+    // Insert the new comment into the "tickets_comments" table
+    $stmt = $pdo->prepare('INSERT INTO tickets_comments (ticket_id, msg) VALUES (?, ?)');
+    $stmt->execute([ $_GET['id'], $_POST['msg'] ]);
+    header('Location: view.php?id=' . $_GET['id']);
+    exit;
+}
+
+$stmt = $pdo->prepare('SELECT * FROM tickets_comments WHERE ticket_id = ? ORDER BY created DESC');
+$stmt->execute([ $_GET['id'] ]);
+$comments = $stmt->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+<?=template_header('Ticket')?>
+
+<div class="content view">
+
+	<h2><?=htmlspecialchars($ticket['title'], ENT_QUOTES)?> <span class="<?=$ticket['status']?>">(<?=$ticket['status']?>)</span></h2>
+
+    <div class="ticket">
+        <p class="created"><?=date('F dS, G:ia', strtotime($ticket['created']))?></p>
+        <p class="msg"><?=nl2br(htmlspecialchars($ticket['msg'], ENT_QUOTES))?></p>
+    </div>
+
+    <div class="btns">
+        <a href="view.php?id=<?=$_GET['id']?>&status=closed" class="btn red">Close</a>
+        <a href="view.php?id=<?=$_GET['id']?>&status=resolved" class="btn">Resolve</a>
+    </div>
+
+    <div class="comments">
+        <?php foreach($comments as $comment): ?>
+        <div class="comment">
+            <div>
+                <i class="fas fa-comment fa-2x"></i>
+            </div>
+            <p>
+                <span><?=date('F dS, G:ia', strtotime($comment['created']))?></span>
+                <?=nl2br(htmlspecialchars($comment['msg'], ENT_QUOTES))?>
+            </p>
+        </div>
+        <?php endforeach; ?>
+        <form action="" method="post">
+            <textarea name="msg" placeholder="Enter your comment..."></textarea>
+            <input type="submit" value="Post Comment">
+        </form>
+    </div>
+
+</div>
+
+<?=template_footer()?>
